2385e6067b71f835c2b1a90631af39e1aedbf2ca
matsukubo <matsukubo@irvinesystems.co.jp>
2013-05-07 15:51:14 +0900

    Initial Commit

---
+++ BounceBall/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  BounceBall
+//
+//  Created by Yuto Matsukubo on 2013/05/07.
+//  Copyright Yuto Matsukubo 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ BounceBall/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BounceBall
+//
+//  Created by Yuto Matsukubo on 2013/05/07.
+//  Copyright Yuto Matsukubo 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
